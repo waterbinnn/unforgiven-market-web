a7b6c2e0b0375697093e72797cb5c8c9c0d53402
Subin Lee <ewaterbinn@gmail.com>
2023-06-15 15:40:19 +0900

    feat: 상품 디테일 페이지 작성

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,8 @@ import { productManage } from "@/api/productManage";
 import Image from "next/image";
 import classNames from "classnames/bind";
 import styles from "../products.module.scss";
+import { Button } from "@/components";
+import CountItem from "../components/CountItem";
 
 const cx = classNames.bind(styles);
 
@@ -12,8 +14,8 @@ export const ProductDetail = async ({ params }: any) => {
 
   return (
     <main className={cx("container")}>
-      <h1>상품 디테일</h1>
-      <section>
+      <section className={cx("detail-container")}>
+        <h2 className={cx("visually-hidden")}>상품상세페이지</h2>
         {data && (
           <>
             <Image
@@ -23,8 +25,59 @@ export const ProductDetail = async ({ params }: any) => {
               height={500}
               placeholder={"blur"}
               blurDataURL={"/assets/default_img.png"}
+              className={cx("image")}
             />
-            <h2>{data.product_name}</h2>
+
+            {/* 상품정보 */}
+            <div className={cx("detail-data-container")}>
+              <div className={cx("detail-info-wrap")}>
+                <span className={cx("store")}>{data.store_name}</span>
+                <dl className={cx("info-wrap")}>
+                  <div className={cx("info-data")}>
+                    <dt className={cx("visually-hidden")}>product name</dt>
+                    <dd className={cx("info-name")}>{data.product_name}</dd>
+                  </div>
+                  <div className={cx("info-data")}>
+                    <dt className={cx("info-title")}>Price</dt>
+                    <dd className={cx("info-price")}>
+                      ￦ {data.price.toLocaleString()}
+                    </dd>
+                  </div>
+                </dl>
+                <span className={cx("shipping-fee")}>
+                  {data.shipping_fee > 0
+                    ? `택배배송/${data.shipping_fee.toLocaleString()}원`
+                    : "택배배송/무료배송"}
+                </span>
+              </div>
+
+              {/* 수량 컴포넌트  */}
+              <div className={cx("count")}>
+                <CountItem stock={data.stock} />
+              </div>
+
+              {/* 총 수량  */}
+              <div className={cx("total-wrap")}>
+                <h3 className={cx("title")}>total</h3>
+                <div className={cx("total-num-wrap")}>
+                  <span className={cx("data-title")}>Total Item</span>
+                  <strong className={cx("data-num")}>{data.stock}</strong>
+                </div>
+                <strong className={cx("data-fee")}>
+                  ￦ {data.price.toLocaleString()}
+                </strong>
+              </div>
+
+              {/* 버튼 섹션  */}
+              <div className={cx("button-wrap")}>
+                <Button color="yellow" size="l" width="60%">
+                  ORDER
+                </Button>
+                <Button color="blue" size="l" width="40%">
+                  CART
+                </Button>
+              </div>
+            </div>
           </>
         )}
       </section>
