a6ab19009e505da5ee01d816c634b20f359219cb
Subin Lee <ewaterbinn@gmail.com>
2023-06-19 18:37:37 +0900

    fix: 상품 디테일 로직 수정 및 리액트 쿼리 작성

---
+++ src/app/products/[id]/Product.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import React from "react";
+import CountItem from "../components/CountItem";
+import { Button } from "@/components";
+
+import classNames from "classnames/bind";
+import styles from "../products.module.scss";
+import { ProductListType } from "@/types/productManage";
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  data: ProductListType;
+}
+
+export const Product = ({ data }: Props) => {
+  return (
+    <>
+      <Image
+        src={data.image}
+        alt={data.product_name}
+        width={500}
+        height={500}
+        placeholder={"blur"}
+        blurDataURL={"/assets/default_img.png"}
+        className={cx("image")}
+      />
+
+      {/* 상품정보 */}
+      <div className={cx("detail-data-container")}>
+        <div className={cx("detail-info-wrap")}>
+          <span className={cx("store")}>{data.store_name}</span>
+          <dl className={cx("info-wrap")}>
+            <div className={cx("info-data")}>
+              <dt className={cx("visually-hidden")}>data name</dt>
+              <dd className={cx("info-name")}>{data.product_name}</dd>
+            </div>
+            <div className={cx("info-data")}>
+              <dt className={cx("info-title")}>Price</dt>
+              <dd className={cx("info-price")}>
+                ￦ {data.price.toLocaleString()}
+              </dd>
+            </div>
+          </dl>
+          <span className={cx("shipping-fee")}>
+            {data.shipping_fee > 0
+              ? `택배배송/${data.shipping_fee.toLocaleString()}원`
+              : "택배배송/무료배송"}
+          </span>
+        </div>
+
+        {/* 수량 컴포넌트  */}
+        <CountItem stock={data.stock} price={data.price} />
+
+        {/* 버튼 섹션  */}
+        <div className={cx("button-wrap")}>
+          <Button color="yellow" size="l" width="60%">
+            ORDER
+          </Button>
+          <Button color="blue" size="l" width="40%">
+            CART
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,85 +1,38 @@
+"use client";
 import { productManage } from "@/api/productManage";
-import Image from "next/image";
+
 import classNames from "classnames/bind";
 import styles from "../products.module.scss";
-import { Button } from "@/components";
-import CountItem from "../components/CountItem";
+import Product from "./Product";
+import { useQuery } from "@tanstack/react-query";
 
 const cx = classNames.bind(styles);
 
-export const ProductDetail = async ({ params }: any) => {
-  const id = params.id;
+const INIT_URL = "https://openmarket.weniv.co.kr/products/";
 
-  const { data } = await productManage.getProductDetail(id);
+const getDetail = async (id: string) => {
+  const res = await fetch(`${INIT_URL}${id}`);
+  const data = await res.json();
+  return data;
+};
+
+export const ProductDetail = async ({ params }: any) => {
+  const id = params.id.toString();
+
+  // const product = await getDetail(id);
+  const { isLoading, isError, data } = useQuery(["product"], () =>
+    getDetail(id)
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
+  if (isError) return <div>Error!</div>;
 
   return (
     <main className={cx("container")}>
       <section className={cx("detail-container")}>
         <h2 className={cx("visually-hidden")}>상품상세페이지</h2>
-        {data && (
-          <>
-            <Image
-              src={data.image}
-              alt={data.product_name}
-              width={500}
-              height={500}
-              placeholder={"blur"}
-              blurDataURL={"/assets/default_img.png"}
-              className={cx("image")}
-            />
-
-            {/* 상품정보 */}
-            <div className={cx("detail-data-container")}>
-              <div className={cx("detail-info-wrap")}>
-                <span className={cx("store")}>{data.store_name}</span>
-                <dl className={cx("info-wrap")}>
-                  <div className={cx("info-data")}>
-                    <dt className={cx("visually-hidden")}>product name</dt>
-                    <dd className={cx("info-name")}>{data.product_name}</dd>
-                  </div>
-                  <div className={cx("info-data")}>
-                    <dt className={cx("info-title")}>Price</dt>
-                    <dd className={cx("info-price")}>
-                      ￦ {data.price.toLocaleString()}
-                    </dd>
-                  </div>
-                </dl>
-                <span className={cx("shipping-fee")}>
-                  {data.shipping_fee > 0
-                    ? `택배배송/${data.shipping_fee.toLocaleString()}원`
-                    : "택배배송/무료배송"}
-                </span>
-              </div>
-
-              {/* 수량 컴포넌트  */}
-              <div className={cx("count")}>
-                <CountItem stock={data.stock} />
-              </div>
-
-              {/* 총 수량  */}
-              <div className={cx("total-wrap")}>
-                <h3 className={cx("title")}>total</h3>
-                <div className={cx("total-num-wrap")}>
-                  <span className={cx("data-title")}>Total Item</span>
-                  <strong className={cx("data-num")}>{data.stock}</strong>
-                </div>
-                <strong className={cx("data-fee")}>
-                  ￦ {data.price.toLocaleString()}
-                </strong>
-              </div>
-
-              {/* 버튼 섹션  */}
-              <div className={cx("button-wrap")}>
-                <Button color="yellow" size="l" width="60%">
-                  ORDER
-                </Button>
-                <Button color="blue" size="l" width="40%">
-                  CART
-                </Button>
-              </div>
-            </div>
-          </>
-        )}
+        <Product data={data} />
       </section>
     </main>
   );
