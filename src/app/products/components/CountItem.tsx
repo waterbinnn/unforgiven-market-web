82c7f1997aa69eb8d3f31af1b41846fc75d12cf7
Subin Lee <ewaterbinn@gmail.com>
2023-06-19 18:38:02 +0900

    fix: 수량 로직 수정

--- src/app/products/components/CountItem.tsx
+++ src/app/products/components/CountItem.tsx
@@ -2,24 +2,30 @@
 
 import { Count } from "@/components";
 import { use, useEffect, useState } from "react";
+import classNames from "classnames/bind";
+
+import styles from "../products.module.scss";
+
+const cx = classNames.bind(styles);
 
 interface Props {
   stock: number;
+  price: number;
 }
 
-export const CountItem = ({ stock }: Props) => {
-  const [count, setCount] = useState<number>(0);
-  const [isMinusDisable, setMinusDisable] = useState<boolean>(true);
+export const CountItem = ({ stock, price }: Props) => {
+  const [count, setCount] = useState<number>(1);
+  const [isMinusDisable, setMinusDisable] = useState<boolean>(false);
   const [isPlusDisable, setPlusDisable] = useState<boolean>(false);
 
   useEffect(() => {
-    if (count === 0) {
+    if (count === 1) {
       setMinusDisable(true);
     }
   }, [count]);
 
   const handleMinus = () => {
-    if (count === 0) {
+    if (count === 1) {
       setMinusDisable(true);
     } else {
       setCount(count - 1);
@@ -32,6 +38,7 @@ export const CountItem = ({ stock }: Props) => {
       alert("재고가 소진되었습니다.");
       setCount(count - 1);
       setPlusDisable(true);
+      setPlusDisable(false);
     } else {
       setCount(count + 1);
       setPlusDisable(false);
@@ -40,7 +47,7 @@ export const CountItem = ({ stock }: Props) => {
   };
 
   return (
-    <>
+    <div className={cx("count")}>
       <Count
         count={count}
         handleMinus={handleMinus}
@@ -48,7 +55,17 @@ export const CountItem = ({ stock }: Props) => {
         isMinusDisable={isMinusDisable}
         isPlusDisable={isPlusDisable}
       />
-    </>
+      <div className={cx("total-wrap")}>
+        <h3 className={cx("title")}>total</h3>
+        <div className={cx("total-num-wrap")}>
+          <span className={cx("data-title")}>Total Item</span>
+          <strong className={cx("data-num")}>{count}</strong>
+        </div>
+        <strong className={cx("data-fee")}>
+          ￦ {(price * count).toLocaleString()}
+        </strong>
+      </div>
+    </div>
   );
 };
 
