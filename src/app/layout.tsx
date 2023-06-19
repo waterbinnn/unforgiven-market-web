50ea4a4a3a996d6d773fd1c45b701cab18988308
Subin Lee <ewaterbinn@gmail.com>
2023-06-19 18:33:29 +0900

    feat: react-query provider, portal-wrap 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import Head from "./head";
 import "../styles/globals.scss";
 import { GNBLayout } from "@/components";
+import Providers from "@/utils/provider";
 
 export default function RootLayout({
   children,
@@ -9,11 +10,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
   return (
     <html>
       <Head />
       <body>
-        <GNBLayout>{children}</GNBLayout>
+        <div id={"portal-wrap"} />
+        <Providers>
+          <GNBLayout>{children}</GNBLayout>
+        </Providers>
       </body>
     </html>
   );
