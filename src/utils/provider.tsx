1a29a2f168dff25e5f0d74778d594715fa76fbbb
Subin Lee <ewaterbinn@gmail.com>
2023-06-19 18:39:31 +0900

    feat: react query 사용 위한 로직 작성

---
+++ src/utils/provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+export const Providers = ({ children }: React.PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
