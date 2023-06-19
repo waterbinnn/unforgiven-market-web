ad6ddeedadcfb11791208ec6161357b277408435
Subin Lee <ewaterbinn@gmail.com>
2023-06-19 18:36:02 +0900

    remove: 불필요한 import 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 export const dynamic = "force-dynamic"; // this is the fix of params
 
-import { Hydrate, dehydrate } from "@tanstack/react-query";
-import ProductList from "./products/components/ProductList";
-import getQueryClient from "@/utils/getQueryClient";
 import ProductPage from "./products/page";
 
 export default async function Home() {
-  const queryClient = getQueryClient();
-  const dehydrateState = dehydrate(queryClient);
-
   return (
-    <Hydrate state={dehydrateState}>
-      <main>
-        {/* @ts-expect-error Server Component */}
-        <ProductPage />
-      </main>
-    </Hydrate>
+    <main>
+      {/* @ts-expect-error Server Component */}
+      <ProductPage />
+    </main>
   );
 }
