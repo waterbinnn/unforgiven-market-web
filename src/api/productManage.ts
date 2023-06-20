45bcf763a188ec5f741bc27f275ce4dbc5c1fc18
Subin Lee <ewaterbinn@gmail.com>
2023-06-20 16:24:49 +0900

    refactor: axios에서 fetch 로 함수 수정

--- src/api/productManage.ts
+++ src/api/productManage.ts
@@ -10,25 +10,29 @@ interface ProductManage {
    * @description 전체 상품 리스트 조회
    * @name productManage.getProductList
    * @method {GET}
-   * @return {AxiosPromise<ProductDataListType>}
+   * @return {ProductDataListType}
    **/
-  // readonly getProductList: (
-  //   pageParam: string
-  // ) => AxiosPromise<ProductDataListType>;
+  readonly getProductList: () => Promise<ProductDataListType>;
 
   /**
    * @description 상품 디테일 조회
    * @name productManage.getProductDetail
    * @method {GET}
-   * @return {Promise<ProductListType>}
+   * @return {ProductListType}
    **/
   readonly getProductDetail: (id: string) => Promise<ProductListType>;
 }
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const productManage: ProductManage = {
-  // getProductDetail: (id: string) => {
-  //   return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/products/${id}`);
-  // },
+  getProductList: async () => {
+    const res = await fetch(`${baseUrl}/products/`);
+    return res.json();
+  },
+  getProductDetail: async (id: string) => {
+    const res = await fetch(`${baseUrl}/products/${id}`);
+    return res.json();
+  },
 };
 
 export { productManage };
