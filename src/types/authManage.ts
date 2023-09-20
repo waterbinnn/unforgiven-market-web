e928c2b1942c98c8748ef3262e76166d3742f82e
Subin Lee <ewaterbinn@gmail.com>
2023-09-20 18:18:56 +0900

    feat: 로그인 타입 작성

--- src/types/authManage.ts
+++ src/types/authManage.ts
@@ -1,4 +1,6 @@
+// <--- signup --->
 interface BuyerSignupReq {
+  //구매자 회원가입 request
   username: string; // 아이디
   password: string;
   password2: string;
@@ -7,6 +9,7 @@ interface BuyerSignupReq {
 }
 
 interface BuyerSignupRes {
+  //구매자 회원가입 response
   name: string;
   phone_number: string;
   type: string;
@@ -14,18 +17,33 @@ interface BuyerSignupRes {
 }
 
 interface SellerSignupReq extends BuyerSignupReq {
+  //판매자 회원가입 request
   company_registration_number: string;
   store_name: string;
 }
 
 interface SellerSignupRes extends BuyerSignupRes {
+  //판매자 회원가입 response
   company_registration_number: string;
   store_name: string;
 }
 
+// <--- signIn --->
+interface SignInReq {
+  username: string;
+  password: string;
+  login_type: "BUYER" | "SELLER"; // BUYER : 일반 구매자, SELLER : 판매자
+}
+interface SignInRes {
+  id: number;
+  token: string;
+  user_type: "BUYER" | "SELLER";
+}
 export type {
   BuyerSignupReq,
   BuyerSignupRes,
   SellerSignupReq,
   SellerSignupRes,
+  SignInReq,
+  SignInRes,
 };
