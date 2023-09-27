8a1ae850292598fdd20b8653c90134542a80080d
Subin Lee <ewaterbinn@gmail.com>
2023-09-27 15:36:00 +0900

    fix: next auth type 수정

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,10 +2,8 @@ import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: number;
-      token: string;
-      user_type: string;
-    };
+    id: number;
+    token: string;
+    user_type: string;
   }
 }
