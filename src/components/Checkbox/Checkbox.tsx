71f631da0a83d1c9747dff4606152b0a89f35625
Subin Lee <ewaterbinn@gmail.com>
2023-09-27 15:08:35 +0900

    feat: 체크박스 컴포넌트 작성

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,19 @@
+import classNames from "classnames/bind";
+import styles from "./Checkbox.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  isChecked: boolean;
+  onClick: () => void;
+}
+
+export const Checkbox = ({ isChecked, onClick }: Props) => {
+  return (
+    <button
+      type="button"
+      className={cx("checkbox", { active: isChecked })}
+      onClick={onClick}
+    />
+  );
+};
