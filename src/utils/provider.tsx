f7f656b76f8863b7dfe5fd093637fbda407464f7
Subin Lee <ewaterbinn@gmail.com>
2023-12-18 23:10:41 +0900

    feat: 리액트쿼리 하이드레이션 작성

--- src/utils/provider.tsx
+++ src/utils/provider.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 import { SessionProvider } from "next-auth/react";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
@@ -10,8 +11,8 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
         <ReactQueryDevtools initialIsOpen={false} />
-        {children}
       </QueryClientProvider>
     </SessionProvider>
   );
