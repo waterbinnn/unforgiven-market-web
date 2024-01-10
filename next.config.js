54a8fece45635ff6c690b8381d4a1241d0084f72
Subin Lee <ewaterbinn@gmail.com>
2024-01-10 22:14:09 +0900

    fix: anrecognized key 제거, lint

--- next.config.js
+++ next.config.js
@@ -1,34 +1,31 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ["openmarket.weniv.co.kr"],
+    domains: ['openmarket.weniv.co.kr'],
   },
   typescript: {},
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
   },
-  distDir: ".next",
+  distDir: '.next',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     }),
-      config.resolve.extensions.push(".ts", ".tsx");
+      config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, "src/styles")],
+    includePaths: [path.join(__dirname, 'src/styles')],
     prependData: `@import "./common/_mixins.scss"; @import "./common/_variables.scss";`,
   },
   trailingSlash: false,
   swcMinify: true,
   compiler: {
-    reactRemoveProperties: { properties: ["^data-cy$"] },
+    reactRemoveProperties: { properties: ['^data-cy$'] },
   },
 };
