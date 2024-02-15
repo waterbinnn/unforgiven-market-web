de31261ee69dd761901898e1061bf98b008f978d
Subin Lee <ewaterbinn@gmail.com>
2024-02-16 00:24:19 +0900

    refactor: 데이터 패칭, 하이드레이션 제거

--- src/app/(Routers)/cart/page.tsx
+++ src/app/(Routers)/cart/page.tsx
@@ -1,27 +1,10 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
-import getQueryClient from '@/utils/getQueryClient';
-import { cartManage } from '@/service';
-import { Hydrate, dehydrate } from '@tanstack/react-query';
 import { CartList } from '@/containers';
+import { getCartList } from '@/actions/CartListActions';
 
 const Cart = async () => {
-  const session = await getServerSession(authOptions);
-  const token = session?.token;
-  if (!token) {
-    return;
-  }
+  const { data } = await getCartList();
 
-  //pre-fetching
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(['cartList'], () => cartManage.getList(token));
-  const dehydrateState = dehydrate(queryClient);
-
-  return (
-    <Hydrate state={dehydrateState}>
-      <CartList token={token} />
-    </Hydrate>
-  );
+  return <CartList data={data} />;
 };
 
 export default Cart;
