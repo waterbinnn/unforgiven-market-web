6ec9ad72436ec5d42090af8e9fb20e6e07b0e32a
Subin Lee <ewaterbinn@gmail.com>
2024-02-16 00:24:35 +0900

    fix: 프리패칭 제거

--- src/app/(Routers)/products/page.tsx
+++ src/app/(Routers)/products/page.tsx
@@ -1,18 +1,7 @@
-import { Hydrate, dehydrate } from '@tanstack/react-query';
-import getQueryClient from '@/utils/getQueryClient';
-import { productManage } from '@/service/productManage';
 import { ProductList } from '@/containers';
 
 const ProductPage = async () => {
-  const queryClient = getQueryClient();
-  await queryClient.prefetchInfiniteQuery(['productsList'], () => productManage.getProductList());
-  const dehydrateState = dehydrate(queryClient);
-
-  return (
-    <Hydrate state={dehydrateState}>
-      <ProductList />
-    </Hydrate>
-  );
+  return <ProductList />;
 };
 
 export default ProductPage;
