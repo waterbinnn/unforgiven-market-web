df28edbaab4ea192ae3f63a54ea2c8b45e767248
Subin Lee <ewaterbinn@gmail.com>
2024-02-24 00:49:41 +0900

    refactor: 데이터 패칭 방식 리팩토링 및 적용
    리액트 쿼리와 서버사이드 상충으로 인한 버그 발생으로 서버액션만 작성하여 데이터 패칭

--- src/app/(Routers)/cart/page.tsx
+++ src/app/(Routers)/cart/page.tsx
@@ -1,26 +1,38 @@
 import { CartList } from '@/containers';
 
-import { getCartList } from '@/actions/CartListActions';
-import { getProductDetail } from '@/actions/ProductActions';
-import { getServerSession } from 'next-auth';
+import { getCartList, getProductDetail } from '@/actions';
 
 const Cart = async () => {
-  // const { data: carts } = await getCartList();
-  const session = await getServerSession();
-  if (!session) {
+  const { data: carts } = await getCartList();
+
+  if (!carts) {
     return;
   }
-  const token = session.token;
-  // if (!carts) {
-  //   return;
-  // }
-  // const ids = carts.results.map((v) => v.product_id.toString());
+  const ids = carts.results.map((v) => v.product_id.toString());
 
-  // const detailPromises = ids.map((v: string) => getProductDetail(v));
-  // const detail = await Promise.all(detailPromises);
+  const detailPromises = ids.map((v: string) => getProductDetail(v));
+  const detail = await Promise.all(detailPromises);
+  if (!detail) {
+    return;
+  }
 
-  // return <CartList carts={carts} detail={detail} />;
-  return <CartList token={token} />;
+  const quantity = carts.results.map((item) => item.quantity);
+  const payments = detail.map((item) => ({
+    price: Number(item.data?.price),
+    shipping: Number(item.data?.shipping_fee),
+  }));
+
+  const totalPrice = payments.reduce((sum, item, index) => sum + item.price * quantity[index], 0);
+  const totalShippingFee = payments.reduce((sum, item) => sum + item.shipping, 0);
+
+  return (
+    <CartList
+      carts={carts}
+      detail={detail}
+      totalPrice={totalPrice}
+      totalShippingFee={totalShippingFee}
+    />
+  );
 };
 
 export default Cart;
--- src/app/(Routers)/products/[id]/page.tsx
+++ src/app/(Routers)/products/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Error from '@/app/error';
+import { getProductDetail } from '@/actions';
 import { ProductDetail } from '@/containers';
 
 interface Props {
@@ -8,7 +8,11 @@ interface Props {
 const ProductDetailPage = async ({ params }: Props) => {
   const productId = params.id.toString();
 
-  return <ProductDetail productId={productId} />;
+  const { data: detail } = await getProductDetail(productId);
+  if (!detail) {
+    return;
+  }
+  return <ProductDetail detail={detail} />;
 };
 
 export default ProductDetailPage;
