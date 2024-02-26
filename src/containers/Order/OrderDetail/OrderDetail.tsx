b30d48536066dd8288b9c457a208cb9bc5275092
Subin Lee <ewaterbinn@gmail.com>
2024-02-26 23:40:33 +0900

    feat: 주문하기 페이지 퍼블리싱

--- src/containers/Order/OrderDetail/OrderDetail.tsx
+++ src/containers/Order/OrderDetail/OrderDetail.tsx
@@ -6,38 +6,34 @@ const cx = classNames.bind(styles);
 
 export const OrderDetail = () => {
   return (
-    <li className={cx('order-detail-wrap')}>
-      <Image
-        width={100}
-        height={100}
-        src={'/assets/default_img.png'}
-        alt="product name"
-        placeholder={'blur'}
-        blurDataURL={'/assets/default_img.png'}
-        loading="lazy"
-      />
-      <div className={cx('detail-info-container')}>
-        <dl className={cx('detail-info-wrap')}>
-          <div className={cx('info-wrap')}>
-            <div>
-              <dt className={cx('visually-hidden')}>store name</dt>
-              <dd className={cx('store')}>우당탕탕 스토어</dd>
-
-              <dt className={cx('visually-hidden')}>product name</dt>
-              <dd className={cx('info-title')}>언포기븐 앨범</dd>
-            </div>
-
-            <dt className={cx('title')}>수량</dt>
-            <dd className={cx('info-count')}>수량 1개</dd>
+    <tr className={cx('order-detail-wrap')}>
+      <td className={cx('item-wrap')}>
+        <Image
+          width={100}
+          height={100}
+          src={'/assets/default_img.png'}
+          alt="product name"
+          placeholder={'blur'}
+          blurDataURL={'/assets/default_img.png'}
+          loading="lazy"
+        />
+        <div className={cx('info-wrap')}>
+          <div className={cx('column-wrap')}>
+            <p className={cx('info', 'store')}>{'우당탕탕 스토어'}</p>
+            <p className={cx('info')}>{'언포기븐 앨범'}</p>
           </div>
-
-          <dt className={cx('shipping-fee')}>delivery fee</dt>
-          <dd className={cx('info-fee')}>무료배송</dd>
-
-          <dt className={cx('visually-hidden')}>Price</dt>
-          <dd className={cx('info-price')}>￦ 15,000</dd>
-        </dl>
-      </div>
-    </li>
+          <span className={cx('info', 'count')}>{'수량 1 개'}</span>
+        </div>
+      </td>
+      <td>
+        <p className={cx('info-base')}>-￦ {'1,000'}</p>
+      </td>
+      <td>
+        <p className={cx('info-base')}>￦ {'무료배송'}</p>
+      </td>
+      <td>
+        <p className={cx('info-base', 'price')}>{'￦ 15,000'}</p>
+      </td>
+    </tr>
   );
 };
