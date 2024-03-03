591fac2380feb62d1b0b5c678790b062fe863777
Subin Lee <ewaterbinn@gmail.com>
2024-03-03 19:49:13 +0900

    feat: 주문 완료 페이지 개발

--- src/app/(Routers)/order/complete/page.tsx
+++ src/app/(Routers)/order/complete/page.tsx
@@ -1,10 +1,41 @@
-import { getOrderList } from '@/actions';
+import { getOrderList, getProductDetail } from '@/actions';
 import { OrderList } from '@/containers';
+import { OrderDetailType } from '@/store';
+import { OrderListData } from '@/types';
 
 export const page = async () => {
-  const res = await getOrderList();
+  const { list } = await getOrderList();
 
-  return <OrderList />;
+  interface NewItem {
+    productDetail: OrderDetailType | unknown;
+  }
+
+  let orderDetails: {
+    id: number;
+    listData: OrderListData['results'][0];
+    productDetail: NewItem[];
+  }[] = [];
+
+  await Promise.all(
+    list.map(async (item) => {
+      const itemData = await Promise.all(
+        item.order_items.map(async (orderItem, index) => {
+          let newItem = {
+            id: orderItem,
+            productDetail: {} as OrderDetailType as unknown,
+          };
+
+          let { data } = await getProductDetail(String(orderItem));
+          newItem.productDetail = { ...data, quantity: item.order_quantity[index] };
+
+          return newItem;
+        }),
+      );
+      orderDetails.push({ id: item.order_number, listData: item, productDetail: itemData });
+    }),
+  );
+
+  return <OrderList orderDetails={orderDetails} />;
 };
 
 export default page;
