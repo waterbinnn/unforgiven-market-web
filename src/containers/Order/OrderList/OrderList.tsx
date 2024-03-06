6f6d3c275b64696cf2768d776de4cf5bdc310bbb
Subin Lee <ewaterbinn@gmail.com>
2024-03-06 17:25:04 +0900

    feat: payment label 값 적용

--- src/containers/Order/OrderList/OrderList.tsx
+++ src/containers/Order/OrderList/OrderList.tsx
@@ -8,6 +8,7 @@ import dayjs from 'dayjs';
 import { OrderDetailType } from '@/store';
 import { OrderDetail } from '../OrderDetail';
 import Loading from '@/app/loading';
+import { paymentsOptions } from '../contents';
 
 const cx = classNames.bind(styles);
 
@@ -22,6 +23,7 @@ interface Props {
     productDetail: NewItem[];
   }[];
 }
+
 export const OrderList = ({ orderDetails }: Props) => {
   if (!orderDetails) {
     return <Loading />;
@@ -32,34 +34,39 @@ export const OrderList = ({ orderDetails }: Props) => {
       <h1 className={cx('page-title')}>ORDER LIST</h1>
       <div className={cx('order-container')}>
         {orderDetails &&
-          orderDetails.map((order) => (
-            <div key={order.id} className={cx('item-wrap')}>
-              <div>
-                <OrderTable>
-                  {order.productDetail.map((item, index) => (
-                    <OrderDetail detail={item.productDetail as OrderDetailType} key={index} />
-                  ))}
-                </OrderTable>
-              </div>
+          orderDetails.map((order) => {
+            const paymentMethod = order.listData.payment_method;
+            const paymentOption = paymentsOptions.find((option) => option.value === paymentMethod);
 
-              <div className={cx('order-info-wrap')}>
-                <div className={cx('info-header')}>
-                  <h3>주문 정보</h3>
-                  <p className={cx('time')}>
-                    {dayjs(order.listData.created_at).format('YYYY-MM-DD HH:MM')}
-                  </p>
+            return (
+              <div key={order.id} className={cx('item-wrap')}>
+                <div>
+                  <OrderTable>
+                    {order.productDetail.map((item, index) => (
+                      <OrderDetail detail={item.productDetail as OrderDetailType} key={index} />
+                    ))}
+                  </OrderTable>
                 </div>
-                <InfoLayout label="주문 번호" content={order.listData.order_number} />
-                <InfoLayout label="배송지" content={order.listData.address} />
-                <InfoLayout label="배송메세지" content={order.listData.address_message} />
-                <InfoLayout label="결제 정보" content={order.listData.payment_method} />
-                <InfoLayout
-                  label="진행 상태"
-                  content={order.listData.delivery_status === 'COMPLETE_PAYMENT' && '결제 완료'}
-                />
+
+                <div className={cx('order-info-wrap')}>
+                  <div className={cx('info-header')}>
+                    <h3>주문 정보</h3>
+                    <p className={cx('time')}>
+                      {dayjs(order.listData.created_at).format('YYYY-MM-DD HH:MM')}
+                    </p>
+                  </div>
+                  <InfoLayout label="주문 번호" content={order.listData.order_number} />
+                  <InfoLayout label="배송지" content={order.listData.address} />
+                  <InfoLayout label="배송메세지" content={order.listData.address_message} />
+                  <InfoLayout label="결제 정보" content={paymentOption?.label} />
+                  <InfoLayout
+                    label="진행 상태"
+                    content={order.listData.delivery_status === 'COMPLETE_PAYMENT' && '결제 완료'}
+                  />
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
       </div>
     </div>
   );
