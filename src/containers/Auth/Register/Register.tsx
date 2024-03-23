f73ab0b25a7719da55fafe003f5ee8134358c390
Subin Lee <ewaterbinn@gmail.com>
2024-03-23 22:50:53 +0900

    fix: 클라이언트 액션 함수 적용

--- src/containers/Auth/Register/Register.tsx
+++ src/containers/Auth/Register/Register.tsx
@@ -7,9 +7,9 @@ import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { Button, Input } from '@/components';
 import { useState } from 'react';
-import { authManage } from '@/service';
 import { BuyerSignupReq, SellerSignupReq } from '@/types';
-import { signIn } from 'next-auth/react';
+import { checkCompanyNumber, checkIdValidation, buyerSignup, sellerSignup } from '@/actions';
+import { message } from 'antd';
 
 const cx = classNames.bind({ ...styles, ...CommonStyle });
 
@@ -52,95 +52,96 @@ export const Register = ({ type }: Props) => {
   //아이디(username) 유효성 검사위한 함수
   const onCheckIdValid = async () => {
     const username = getValues('username');
+    const { data, success } = await checkIdValidation(username);
 
-    try {
-      const { data } = await authManage.checkIdValid(username);
-
-      if ('FAIL_Message' in data) {
-        setCheckIdValid('error');
-        setCheckIdValidMsg(data.FAIL_Message);
+    if (success) {
+      if (username.length > 1 && username.match(idRule)) {
+        setCheckIdValid('valid');
+        setCheckIdValidMsg('사용 가능한 아이디 입니다.');
       } else {
-        if (username.length > 1 && username.match(idRule)) {
-          setCheckIdValid('valid');
-          setCheckIdValidMsg('사용 가능한 아이디 입니다.');
-        } else {
-          setCheckIdValid('error');
-          setCheckIdValidMsg('20자 이내의 영어 소문자, 대문자, 숫자를 조합해 입력해주세요.');
-        }
+        setCheckIdValid('error');
+        setCheckIdValidMsg('20자 이내의 영어 소문자, 대문자, 숫자를 조합해 입력해주세요.');
       }
-    } catch (err) {
-      console.log(err);
+    } else {
+      setCheckIdValid('error');
+      setCheckIdValidMsg(data.response.data.FAIL_Message);
     }
   };
 
   //사업자등록번호(company_registration_number) 유효성 검사 위한 함수
   const onCheckStoreNameValid = async () => {
     const company_registration_number = getValues('company_registration_number');
-    try {
-      const { data } = await authManage.checkCompanyNumberValid(company_registration_number);
-      if ('FAIL_Message' in data) {
-        setRegistrationNumberValid('error');
-        setRegistrationNumberMsg(data.FAIL_Message as string);
-      } else {
-        setRegistrationNumberValid('valid');
-        setRegistrationNumberMsg(data.statusText);
-      }
-    } catch (err) {
-      console.log(err);
+
+    const { data, success } = await checkCompanyNumber(company_registration_number);
+    if (success) {
+      setRegistrationNumberValid('valid');
+      setRegistrationNumberMsg('사용할 수 있는 번호입니다.');
+    } else {
+      setRegistrationNumberValid('error');
+      setRegistrationNumberMsg(data.response.data.FAIL_Message);
     }
   };
 
-  // signUp 버튼 클릭시 동작하는 함수
-  const onSubmit = async (data: any) => {
-    const formData: BuyerSignupReq = {
-      //구매자 회원가입 req data
+  const onFinish = async (data: any) => {
+    const createFormData = (additionalData: any = {}) => ({
       username: data.username,
       password: data.password,
       password2: data.password2,
       phone_number: '0' + parseInt(data.firstNum) + data.middleNum + data.lastNum,
       name: data.name,
-    };
+      ...additionalData,
+    });
 
-    const sellerFormData: SellerSignupReq = {
-      //판매자 회원가입 req data
-      ...formData,
+    const formData: BuyerSignupReq = createFormData();
+    const sellerFormData: SellerSignupReq = createFormData({
       store_name: data.store_name,
       company_registration_number: data.company_registration_number,
-    };
+    });
 
     if (checkIdValid === 'valid' && isAgree) {
-      try {
-        if (type === 'BUYER') {
-          //구매자 회원가입 로직
-          const { data } = await authManage.buyerSignup(formData);
-
-          if (Object.values(data)[0] === '해당 사용자 전화번호는 이미 존재합니다.') {
-            setPhoneError(data.phone_number);
-            setValue('middleNum', '');
-            setValue('lastNum', '');
-          } else if (Object.values(data).length === 4) {
-            signIn(undefined, { callbackUrl: '/' });
-          }
-        } else {
-          //판매자 회원가입 로직
-          const res = await authManage.sellerSignup(sellerFormData);
-          if (data.store_name) {
-            setValue('store_name', '');
-            setError('store_name', { type: 'custom', message: data.store_name });
-          } else {
-            handleRouter('/signin/seller');
+      if (type === 'BUYER') {
+        //구매자 회원가입 로직
+        const { success, res } = await buyerSignup(formData);
+        if (!success) {
+          const message = res.response.data;
+          if (message.phone_number) {
+            handlePhoneError(message.phone_number[0]);
           }
         }
-      } catch (error) {
-        console.log('error', error);
+        if (success) {
+          message.success('가입이 완료되었습니다. 로그인 후 사용해 주세요.');
+          handleRouter(`/signin`);
+        }
+      } else {
+        //판매자 회원가입 로직
+        const { success, res } = await sellerSignup(sellerFormData);
+        if (!success) {
+          const message = res.response.data;
+          if (message.store_name) {
+            setValue('store_name', '');
+            setError('store_name', { type: 'custom', message: message.store_name[0] });
+          }
+          if (message.phone_number) {
+            handlePhoneError(message.phone_number[0]);
+          }
+        } else {
+          message.success('가입이 완료되었습니다. 로그인 후 사용해 주세요.');
+          handleRouter('/signin/seller');
+        }
       }
     } else {
       if (!isAgree) {
-        alert('이용약관 및 개인정보처리방침에 동의해 주세요.');
+        message.info('이용약관 및 개인정보처리방침에 동의해 주세요.');
       } else if (checkIdValid === 'none') {
-        alert('Check for ID availability');
+        message.info('Check for ID availability');
       }
     }
+  };
+
+  const handlePhoneError = (message: string) => {
+    setValue('middleNum', '');
+    setValue('lastNum', '');
+    setPhoneError(message);
   };
 
   return (
@@ -149,7 +150,7 @@ export const Register = ({ type }: Props) => {
         <main className={cx('auth-inner-container')}>
           <h1 className={cx('visually-hidden')}>{type} SIGNUP</h1>
 
-          <form className={cx('auth-wrap')} onSubmit={handleSubmit(onSubmit)}>
+          <form className={cx('auth-wrap')} onSubmit={handleSubmit(onFinish)}>
             <h3 className={cx('title', 'top')}>{type}</h3>
             <h3 className={cx('title', 'bottom')}>SIGNUP</h3>
             <div className={cx('inputs-wrap')}>
