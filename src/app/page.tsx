57fb3067523babb42e9f911d58e6f4182f04dad5
Subin Lee <ewaterbinn@gmail.com>
2024-03-23 15:21:25 +0900

    delete: 불필요한 코드 제거, 데이터 클라이언트에서 받는 형식으로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { getProductList } from '@/actions';
 import { ProductList } from '@/containers';
 
 const Main = async () => {
-  const data = await getProductList(1);
-
-  if (!data) {
-    return;
-  }
-  return <ProductList data={data.results} />;
+  return <ProductList />;
 };
 
 export default Main;
