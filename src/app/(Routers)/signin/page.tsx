a0aa3524299b31c87ba956bbcd06430964859501
Subin Lee <ewaterbinn@gmail.com>
2024-03-28 19:56:06 +0900

    feat: suspense 로딩 화면 작성

--- src/app/(Routers)/signin/page.tsx
+++ src/app/(Routers)/signin/page.tsx
@@ -1,7 +1,13 @@
+import Loading from '@/app/loading';
 import { SignIn } from '@/containers';
+import { Suspense } from 'react';
 
 const Login = async () => {
-  return <SignIn type={'BUYER'} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <SignIn type={'BUYER'} />
+    </Suspense>
+  );
 };
 
 export default Login;
