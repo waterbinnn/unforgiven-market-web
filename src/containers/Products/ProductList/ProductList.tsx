f5b21ed758fbe0e370618b12897df4d3f6436cf1
Subin Lee <ewaterbinn@gmail.com>
2024-04-07 16:44:11 +0900

    refactor: 무한스크롤 로직 제거, 더보기 버튼으로 변경

--- src/containers/Products/ProductList/ProductList.tsx
+++ src/containers/Products/ProductList/ProductList.tsx
@@ -4,18 +4,59 @@ import classNames from 'classnames/bind';
 import styles from './ProductList.module.scss';
 
 import { ProductListType } from '@/types';
-import { LoadMore } from './localComponents';
+import { ProductItem } from './localComponents';
+import { useCallback, useState } from 'react';
+import { getProductList } from '@/actions';
+import { Button } from '@/components';
 
 const cx = classNames.bind(styles);
 
 export const ProductList = ({ initialProducts }: { initialProducts: ProductListType[] | null }) => {
+  const [products, setProducts] = useState<ProductListType[]>(
+    initialProducts ? initialProducts : [],
+  );
+  const [page, setPage] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLastPage, setIsLastPage] = useState<boolean>(false);
+
+  const loadMore = useCallback(async () => {
+    setIsLoading(true);
+    const { data, success } = await getProductList(page + 1);
+
+    if (!success) {
+      return;
+    }
+    if (success && data) {
+      setProducts((prev) => [...prev, ...data.results]);
+      setPage(page + 1);
+      setIsLoading(false);
+
+      if (!data.next) {
+        setIsLastPage(true);
+      }
+    }
+  }, [page]);
+
   return (
     <>
       <section className={cx('container')}>
         <h2 className={cx('visually-hidden')}>전체상품목록</h2>
         <ul className={cx('list-container')} role="list">
-          {initialProducts && <LoadMore initialProducts={initialProducts} />}
+          {products?.map((item) => (
+            <ProductItem product={item} key={item.product_id} />
+          ))}
         </ul>
+        <div className={cx('btn-more-wrap')}>
+          {!isLastPage && (
+            <Button size="s" color={'outline'} width={'50%'} onClick={loadMore}>
+              {!isLoading ? (
+                '상품 더보기'
+              ) : (
+                <img src={'/assets/spinner.svg'} width={18} height={18} alt="loading..." />
+              )}
+            </Button>
+          )}
+        </div>
       </section>
     </>
   );
--- src/containers/Products/ProductList/localComponents/index.ts
+++ src/containers/Products/ProductList/localComponents/index.ts
@@ -1,2 +1,1 @@
 export * from './ProductItem';
-export * from './LoadMore';
