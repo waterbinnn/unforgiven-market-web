9b03db580ea967fefcea3ee2cd102c64f62d94ac
Subin Lee <ewaterbinn@gmail.com>
2024-05-08 22:27:38 +0900

    fix: error page ui 수정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@/components';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 interface Props {
@@ -13,11 +14,13 @@ export default function Error({ errorMsg, url }: Props) {
 
   return (
     <div className={'error-container'}>
+      <strong>ERROR</strong>
       <p>Something went wrong!</p>
-      <span>{errorMsg}</span>
-      <Button color="yellow" width="300px" onClick={() => router.push(url)}>
-        TryAgain
-      </Button>
+      <Link href={'/'}>
+        <Button size="m" width="100%">
+          메인 페이지로 이동
+        </Button>
+      </Link>
     </div>
   );
 }
