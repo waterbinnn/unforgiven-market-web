fa072a0aeab87506c277954f309d3cda0249a249
Subin Lee <ewaterbinn@gmail.com>
2024-06-05 14:53:31 +0900

    deps: install and set GA

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,10 @@
+import { GoogleTagManager } from '@next/third-parties/google';
+
 export default function Head() {
   return (
     <>
       <title>UNFORGIVEN | New Market Place</title>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID as string} />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Unforgiven, The Market Place" />
       <link rel="icon" href="/favicon.ico" />
