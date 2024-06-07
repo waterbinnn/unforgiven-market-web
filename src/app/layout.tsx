d5fdd6b311ca6d0932420833c35fb2763dd05798
Subin Lee <ewaterbinn@gmail.com>
2024-06-07 14:41:36 +0900

    delete: 불필요한 코드 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,12 @@ import Providers from '@/utils/provider';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytics';
-import { GoogleTagManager } from '@next/third-parties/google';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
 
   return (
     <html>
-      {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID as string} /> */}
       <Head />
       <GoogleAnalytics />
       <body>
